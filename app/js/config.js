28ec458aed38c85d05937ea810432a4ca5f58e88
Jacob Parra <jacobrpg@gmail.com>
2013-11-29 12:52:17 -0500

    Building Out The Application

--- app/js/config.js
+++ app/js/config.js
@@ -9,6 +9,10 @@ app.config(['$routeProvider',
       .when('/',        { templateUrl: 'views/default.html' })
       .when('/signin',  { templateUrl: 'views/users/signin.html' })
       .when('/signup',  { templateUrl: 'views/users/signup.html' })
+      .when('/nflteams',            { templateUrl: 'views/nfl/list.html'
+                                    , authRequired: true })
+      .when('/nflteams/:nflTeamId', { templateUrl: 'views/nfl/view.html'
+                                    , authRequired: true })
       .otherwise(       { redirectTo: '/' });
     }])
   
@@ -21,6 +25,6 @@ app.config(['$routeProvider',
 
   // your Firebase URL goes here
   // should look something like: https://blahblahblah.firebaseio.com
-  .constant('FBURL', '<<<FIREBASE_URL_GOES_HERE>>>')
+  .constant('FBURL', 'https://fantasy-tut.firebaseio.com')
 
 
